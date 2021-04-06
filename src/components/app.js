3a6672599fd8e93523182c8a4c7aa1488d69033f
Mark <mark.scrudder@gmail.com>
2021-04-06 16:02:42 -0700

    Added basic functionality to App.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import Search from './search.js';
+import Results from './results.js';
+
 
 const App = (props) => {
+  let [ input ] = useState('');
+  let [ records ] = useState([]);
+
+  handleChange = (e) => {
+    this.setState({ input: e.target.value });
+    this.getData();
+  };
+
+  getData = () => {
+    Axios.get(`https://www.boardgamegeek.com/xmlapi2/search?query=${this.state.input}`)
+    .then(
+      (results) => {
+        this.setState({
+          records: results.data,
+        })
+      },
+      (error) => {
+        console.error(error);
+      }
+    )
+  }
 
   return(
     <div>
-      WIP
+      <Search inputHandler={this.handleChange} />
+      <Results data={this.state.records} />
     </div>
   )
 }
